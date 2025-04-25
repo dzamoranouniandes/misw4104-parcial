fb7b966933a23233282f2343308609bec34e7821
d.zmorano <d.zmorano@uniandes.edu.co>
2025-04-25 18:04:19 -0500

    Configuración Bootstrap, adición módulo VehicleModule y environments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'misw4104-parcial-template';
+  title = 'misw4104-parcial';
 }
